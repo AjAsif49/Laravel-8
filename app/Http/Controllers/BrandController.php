1e60e44c24fd9a290078252b43538fa31ade7c09
Aj Asif <54905485+AjAsif49@users.noreply.github.com>
2021-07-30 00:52:03 +0600

    first commit

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -7,6 +7,7 @@ use App\Models\Brand;
 use Illuminate\Support\Carbon;
 
 
+
 class BrandController extends Controller
 {
     public function AllBrand(){
@@ -41,6 +42,71 @@ class BrandController extends Controller
         ]);
 
         return Redirect()->back()->with('success', 'Brand Inserted Successfully');
+
+    }
+
+    public function Edit($id){
+        $brands = Brand::find($id);
+        return view('admin.brand.edit', compact('brands'));
+
+    }
+
+    public function Update(Request $request, $id){
+        $validatedData = $request->validate([
+            'brand_name' => 'required|max:25',
+
+        ],
+        [
+            'brand_name.required'=>'Enter Brand name please',
+            'brand_image.min' =>'Brand longer than 4 characters',
+            
+        ]);
+        $old_image = $request->old_image;
+
+        $brand_image = $request->file('brand_image');
+
+        if($brand_image){
+            
+            $name_generate = hexdec(uniqid());
+            $img_ext = strtolower($brand_image->getClientOriginalExtension());
+            $img_name = $name_generate.'.'.$img_ext;
+            $upload_location = 'image/brand/';
+            $last_img = $upload_location.$img_name;
+            $brand_image -> move($upload_location, $img_name);
+
+            unlink($old_image);
+            Brand::find($id)->update([
+                'brand_name'=> $request->brand_name,
+                'brand_image' => $last_img,
+                'created_at' => Carbon::now()
+            ]);
+
+        // return Redirect()->back()->with('success', 'Brand Updated Successfully');
+        return Redirect()->route('all.brand')->with('success', 'Category updated Successfully');
+
+    }
+
+        else{
+            Brand::find($id)->update([
+                'brand_name'=> $request->brand_name,
+                'created_at' => Carbon::now()
+            ]);
+            // return Redirect()->back()->with('success', 'Brand Updated Successfully');
+            return Redirect()->route('all.brand')->with('success', 'Category updated Successfully');
+
+        }
+
+        
+
+    }
+
+    public function delete($id){
+        $image = Brand::find($id);
+        $old_image = $image->brand_image;
+        unlink($old_image);
+
+        Brand::find($id)->delete();
+        return Redirect()->back()->with('success', 'Brand Deleted Successfully');
 
     }
 }
--- resources/views/admin/brand/index.blade.php
+++ resources/views/admin/brand/index.blade.php
@@ -48,7 +48,7 @@
       <td> {{ carbon\carbon::parse($brand->created_at)->diffForHumans() }} </td>
       <td>
         <a href="{{ url('brand/edit/'.$brand->id) }}" class="btn btn-info">Edit</a>
-        <a href="{{ url('brand/delete/'.$brand->id) }}" class="btn btn-danger">Delete</a>
+        <a href="{{ url('brand/delete/'.$brand->id) }}" onclick="return confirm('Are you Sure to Delete?')" class="btn btn-danger">Delete</a>
       </td>
     </tr>
     @endforeach
